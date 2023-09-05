d32369fe60def321d44d306bb4ecfeae9cfe7bd9
Ishan Shah <ishans@splunk.com>
2023-09-05 15:49:29 +0530

    Added license and dev checks

--- fortindrcloud_consts.py
+++ fortindrcloud_consts.py
@@ -1,3 +1,18 @@
+# File: fortindrcloud_consts.py
+#
+# Copyright (c) 2018-2023 Fortinet Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions
+# and limitations under the License.
+
 TRAINING_ACC = "f6f6f836-8bcd-4f5d-bd61-68d303c4f634"
 MAX_DETECTIONS = 10000
 DEFAULT_LIMIT = 100
